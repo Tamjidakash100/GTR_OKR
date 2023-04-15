6378a25a15c31a1ecfbcf91252c0b6d726acb24d
tamjid <tamjidakash100@gmail.com>
2023-04-15 13:00:40 +0600

    remove comments

--- GTR_OKR/Controllers/CompanyController.cs
+++ GTR_OKR/Controllers/CompanyController.cs
@@ -28,57 +28,7 @@ namespace GTR_OKR.Controllers
             return _userInfo.GetCompanyList();
         }
 
-        //// GET: api/Company/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Company>> GetCompany(int id)
-        //{
-        //  if (_context.Companies == null)
-        //  {
-        //      return NotFound();
-        //  }
-        //    var company = await _context.Companies.FindAsync(id);
-
-        //    if (company == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return company;
-        //}
-
-        //// PUT: api/Company/5
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutCompany(int id, Company company)
-        //{
-        //    if (id != company.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    _context.Entry(company).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!CompanyExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
-
-        // POST: api/Company
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        
         [HttpPost]
         public string PostCompany(Company company)
         {
@@ -86,29 +36,6 @@ namespace GTR_OKR.Controllers
             return msg;
         }
 
-        // DELETE: api/Company/5
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteCompany(int id)
-        //{
-        //    if (_context.Companies == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    var company = await _context.Companies.FindAsync(id);
-        //    if (company == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    _context.Companies.Remove(company);
-        //    await _context.SaveChangesAsync();
-
-        //    return NoContent();
-        //}
-
-        //private bool CompanyExists(int id)
-        //{
-        //    return (_context.Companies?.Any(e => e.Id == id)).GetValueOrDefault();
-        //}
+        
     }
 }
